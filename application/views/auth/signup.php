6207412b65d1d018bc1d0ef48c9446d392cba8db
Antonio Komang Yudistira <komanggt12@gmail.com>
2019-09-26 21:33:57 +0800

    merapihkan view signup

--- application/views/auth/signup.php
+++ application/views/auth/signup.php
@@ -4,20 +4,20 @@
             <h1>Halaman Sign Up</h1>
             <!-- login start -->
             <form action="" method="post">
-            <div class="form-group">
+                <div class="form-group">
                     <label for="nama">nama :</label>
                     <input placeholder="Input Your nama here..." type="text" name="nama" id="nama" class="form-control">
-                    <?= form_error('nama','<small class="text-danger pl-3">','</small>'); ?>
+                    <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="email">email :</label>
                     <input placeholder="Input Your email here..." type="text" name="email" id="email" class="form-control">
-                    <?= form_error('email','<small class="text-danger pl-3">','</small>'); ?>
+                    <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="password">password :</label>
                     <input placeholder="Input Your password here..." type="password" name="password" id="password" class="form-control">
-                    <?= form_error('password','<small class="text-danger pl-3">','</small>'); ?>
+                    <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="password2">Repeat Password</label>
