b0c35aed4f48799bfd561bd3025346cef7bd58a2
Antonio Komang Yudistira <komanggt12@gmail.com>
2019-09-26 19:27:42 +0800

    memperbaiki fungsi login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,43 +9,58 @@ class Auth extends CI_Controller
         parent::__construct();
         $this->load->library('form_validation');
     }
+
+    // halaman auth utama adalah login
     public function index()
     {
         // validasi inputan
-        $this->form_validation->set_rules('usernameLogin', 'Usename', 'trim|required');
-        $this->form_validation->set_rules('passwordLogin', 'Password', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
         if ($this->form_validation->run() == false) {
-            $data['title'] = "Login";
+            $data['title'] = "Login Page";
             $this->load->view('templates/header', $data);
-            $this->load->view('login/index');
+            $this->load->view('auth/login');
             $this->load->view('templates/footer');
         } else {
+            // kalau library form validation berjalan, jalankan method _login yang sudah dibuat dibawah
             $this->_login();
         }
     }
     private function _login()
     {
-        $username = $this->input->post('usernameLogin');
-        $password = $this->input->post('passwordLogin');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
         // var_dump($username);die;
 
-        //query ke database
-        $user = $this->db->get_where('users', ['username' => $username])->row_array();
+        // get data untuk melakukan validasi ke database
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
 
         // jika ada
         if ($user) {
             if (password_verify($password, $user['password'])) {
+                // siapkan data untuk ditampung dalam session
                 $data = [
-                    'iduser' => $user['id'],
-                    'nama' => $user['username'],
-                    'login' => true
+                    'email' => $user['email'],
+                    'nama_user' => $user['nama'],
+                    'login' => true,
+                    'id_user' => $user['id']
                 ];
+                // jalankan method untuk membuat session yang berisi data yang sudah dibuat diatas
                 $this->session->set_userdata($data);
-                redirect('blog1');
+                // tampilkan pesan bahwa login berhasil
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Login Berhasil</div>');
+                // pindahkan ke halaman blog
+                redirect('blog');
             } else {
-                redirect('login');
+                // jika password yang dimasukan oleh user tidak sama dengan yang ada di database
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password yang anda masukan salah!</div>');
+                redirect('auth');
             }
+        } else {
+            // Jika akun tidak ditemukan
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Akun tidak ditemukan :(</div>');
+            redirect('auth');
         }
     }
     public function new()
@@ -72,7 +87,7 @@ class Auth extends CI_Controller
 
     public function logout()
     {
-        $key = array('userid', 'nama', 'login' );
+        $key = array('userid', 'nama', 'login');
         $this->session->unset_userdata($key);
         redirect('home');
     }
