cbe10abd4c64089d38e291a172bf5290bb2fd79f
Antonio Komang Yudistira <komanggt12@gmail.com>
2019-09-29 12:33:00 +0800

    memperbaiki link di main blog page

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -17,7 +17,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Some Title Post</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, soluta aspernatur. Molestiae nam fugit distinctio voluptates, eveniet omnis nobis delectus nostrum saepe quis expedita quas reprehenderit repellat ipsa quaerat dolorem.</p>
-                    <a href="" class="btn btn-primary">Read More...</a>
+                    <a href="<?= base_url('blog/blog1'); ?>" class="btn btn-primary">Read More...</a>
                 </div>
             </div>
         </div>
